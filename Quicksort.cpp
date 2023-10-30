904a84f2b9924769fa5cacec41d9ba7a37730d43
Dan01010011 <136166261+Dan01010011@users.noreply.github.com>
2023-10-30 07:52:03 -0300

    Create Quicksort.cpp

---
+++ Quicksort.cpp
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+void Quicksort();
+
+int Partition();
+
+
+int main() {
+    int A[] = {5, 0, 1, 4, 3, 2};
+
+
+    return 0;
+}
+
+
+void Quicksort() {
+    int l = 0, r = 0, s = 0;
+    
+    if(l < r) {
+        s = Partition();
+    }
+}
+
+int Partition() {
+
+}
