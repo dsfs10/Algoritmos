1e09138bfe023a971e14dc4b0ca2852b8f2f54e2
Dan01010011 <136166261+Dan01010011@users.noreply.github.com>
2023-11-13 07:36:57 -0300

    Create lista1B.cpp

---
+++ lista1B.cpp
@@ -0,0 +1,74 @@
+#include <stdio.h>
+
+void swap(int *a, int pos1, int pos2);
+
+void Quicksort(int *a, int l, int r);
+
+int HoarePartition(int *a, int l, int r);
+
+
+int main() {
+    int n, k, o = 0;
+    scanf("%d %d", &n, &k);
+    int a[n], l = 0, r = n - 1;
+
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &a[i]);
+    }
+
+    Quicksort(a, l, r);
+
+    for(int i = 0; i < n-1; i++) {
+        for(int j = i+1; j < n; j++) {
+            if((a[i] - a[j]) == k) {
+                o++;
+            }
+        }
+    }
+
+    printf("%d\n", o);
+
+    return 0;
+}
+
+
+void swap(int *a, int pos1, int pos2) {
+    int aux = 0;
+
+    aux = a[pos1];
+    a[pos1] = a[pos2];
+    a[pos2] = aux;
+}
+
+void Quicksort(int *a, int l, int r) {
+    int s = 0;
+    if(l < r) {
+        s = HoarePartition(a, l, r);
+        Quicksort(a, l, s - 1);
+        Quicksort(a, s + 1, r);
+    }
+}
+
+int HoarePartition(int *a, int l, int r) {
+    int j, p, i;
+    p = a[l];
+    i = l; // incrementa
+    j = r+1; // decrementa
+
+    do {
+        do {
+            i = i+1;
+        } while(!(a[i] <= p || i >= r)); // para decrescente troca o >= pra <= no a[i] (o i deixa como ta)
+        do {
+            j = j-1;
+        } while(!(a[j] >= p)); // pra decrescente troca o <= pra >=
+        
+        swap(a, i, j);
+    } while(!(i >= j));
+    
+    // Undo last swap when i >= j
+    swap(a, i, j);
+    swap(a ,l, j);
+    
+    return j;
+}
