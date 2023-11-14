0b206b30575446ed366b1482d735a269ae026960
Dan01010011 <136166261+Dan01010011@users.noreply.github.com>
2023-11-13 23:30:03 -0300

    Update and rename Discounts.c to Lista1A.c

--- Lista1A.c
+++ Lista1A.c
@@ -1,40 +1,35 @@
 #include <stdio.h>
 
-void swap(int *a, int pos1, int pos2);
+void swap(long long int *a, int pos1, int pos2);
 
-void Quicksort(int *a, int l, int r);
+void Quicksort(long long int *a, int l, int r);
 
-int Partition(int *a, int l, int r);
+int Partition(long long int *a, int l, int r);
 
 
 
 int main() {
     int n = 0, m = 0;
     scanf("%d", &n); // Definindo o numero de barras
-    int a[n], l = 0, r = n-1;
+    long long int a[n], soma = 0;
+    int l = 0, r = n-1;
 
     for(int i = 0; i < n; i++) {
-        scanf("%d", &a[i]); // Definindo o preco de cada uma das n barras
+        scanf("%lld", &a[i]); // Definindo o preco de cada uma das n barras
+        soma += a[i]; 
     }
 
     Quicksort(a, l, r); // Ordena o array a em ordem crescente
 
     scanf("%d", &m); // Definindo o numero de cupons
-    int q[m], s[m];
+    long long int s[m];
+    int q[m];
 
     for(int i = 0; i < m; i++) {
-        int soma = 0;
         scanf("%d", &q[i]); // Definindo o numero de barras que voce tem que comprar usando o ith cupom para que o mais barato deles seja de graca
-        for(int j = n-1; j >= 0; j--) {
-            if(a[j] != a[n - q[i]]) {    
-                soma += a[j];
-            }    
-        }
-        s[i] = soma; 
-    }
-
-    for(int i = 0; i < m; i++) {
-        printf("%d\n", s[i]);
+        
+        s[i] = (soma - a[n - q[i]]); 
+        printf("%lld\n", s[i]);
     }
     
 
@@ -43,7 +38,7 @@ int main() {
 
 
 
-void swap(int *a, int pos1, int pos2) {
+void swap(long long int *a, int pos1, int pos2) {
     int aux = 0;
 
     aux = a[pos1];
@@ -51,7 +46,7 @@ void swap(int *a, int pos1, int pos2) {
     a[pos2] = aux;
 }
 
-void Quicksort(int *a, int l, int r) {
+void Quicksort(long long int *a, int l, int r) {
     int s = 0;
     if(l < r) {
         s = Partition(a, l, r); // s = split position
@@ -60,7 +55,7 @@ void Quicksort(int *a, int l, int r) {
     }
 }
 
-int Partition(int *a, int l, int r) {
+int Partition(long long int *a, int l, int r) {
     int p, i, j;
     p = a[l];
     i = l; // incrementa
