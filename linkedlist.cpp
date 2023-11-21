074b8c4c82089b6942a850f6c292903666d75167
Dan01010011 <136166261+Dan01010011@users.noreply.github.com>
2023-11-21 15:33:51 -0300

    Create linkedlist.cpp

---
+++ linkedlist.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include "list.h"
+
+using namespace std;
+
+int main() {
+    List* l = create_list();
+    int comando = 0;
+
+    do { // MENU
+        cout << "=============MENU=============\n";
+        cout << "1) Insert\n2) Remove\n3) Move to Start\n4) Move to End\n5) Previous\n6) Next\n7) Print List\n8) Clear\n9) Leave\n.: ";
+        cin >> comando;
+
+        switch(comando) {
+            case 1:
+                int it;
+                cout << "Digite o elemento a ser inserido na lista: ";
+                cin >> it;
+                insert(l, it);
+                break;
+            case 2:
+                remove(l);
+                break;
+            case 3:
+                moveToStart(l);
+                break;
+            case 4:
+                moveToEnd(l);
+                break;
+            case 5:
+                prev(l);
+                break;
+            case 6:
+                next(l);
+                break;
+            case 7:
+                printList(l);
+                break;
+            case 8:
+                //delete (l);
+                //List* l = create_list(tam);
+                break;
+            case 9:
+                cout << "Leaving..." << endl;
+                break;
+            default:
+                cout << "Opcao Invalida" << endl;
+                break;        
+        }
+    } while(comando != 9);
+
+    return 0;
+}
