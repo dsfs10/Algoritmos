56c66b8d99d546351703149f80665f8a8ab10caa
Dan01010011 <136166261+Dan01010011@users.noreply.github.com>
2023-11-21 15:33:13 -0300

    Update linklist.cpp

--- linklist.cpp
+++ linklist.cpp
@@ -4,26 +4,33 @@ using namespace std;
 
 typedef struct link {
     int element; // E type, value stored in this link/node
-    Link next; // Reference to the next link/node
+    Link* next; // Reference to the next link/node
 } Link;
 
 typedef struct list {
-    Link head;
-    Link tail;
-    Link curr;
+    Link* head;
+    Link* tail;
+    Link* curr;
     int cnt; // list size
 } List;
 
-Link create_link(Link nextval) {
-    Link n;
-    n.next = nextval;
+Link* create_link(int it, Link* nextval) {
+    Link* n;
+    n->element = it;
+    n->next = nextval;
+    return n;
+}
+
+Link* create_link(Link* nextval) {
+    Link* n;
+    n->next = nextval;
     return n;
 }
 
 List* create_list() {
-    //List* l = (List *) new int;
+    List* l = (List *) new int;
     l->curr = l->tail = l->head = create_link(NULL); // header node
-    l.cnt ← 0;
+    l->cnt = 0;
     return l;
 }
 
@@ -31,30 +38,55 @@ void clear(List* l) {
 
 }
 
-void insert(List* l, int item) {
-
+void insert(List* l, int it) {
+    l->curr->next = create_link(it, l->curr->next);
+    
+    if(l->tail == l->curr) {
+        l->tail = l->curr->next;
+    }
+    l->cnt++;
 }
 
-void remove(List* l) {
+int remove(List* l) {
+    if(l->curr->next == NULL) {
+        return NULL;
+    }
+    int it = l->curr->next->element;
 
+    if(l->tail == l->curr->next) {
+        l->tail = l->curr;
+    }
+    l->curr->next = l->curr->next->next;
+    l->cnt--;
 }
 
 void moveToStart(List* l) {
-
+    l->curr = l->head;
 }
 
 void moveToEnd(List* l) {
-
+    l->curr = l->tail;
 }
 
 void prev(List* l) {
+    if(l->curr == l->head) {
+        return;
+    }
 
+    Link* temp = l->head;
+
+    while(temp->next != l->curr) {
+        temp = temp->next;
+    }
+    l->curr = temp;
 }
 
 void next(List* l) {
-
+    if(l->curr != l->tail) {
+        l->curr = l->curr->next;
+    }
 }
 
 void printList(List* l) {
-
+    
 }
