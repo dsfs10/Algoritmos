725a3e3105da54d350c38440203a6342fac3c6ef
Dan01010011 <136166261+Dan01010011@users.noreply.github.com>
2023-11-24 07:55:44 -0300

    Update AP4.cpp

--- AP4.cpp
+++ AP4.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <sstream>
 
 using namespace std;
 
@@ -26,8 +27,10 @@ void remove(List* l);
 void prev(List* l);
 void next(List* l);
 void printList(List* l);
+int count(List* l);
 Link* create_link(int it, Link* nextval);
 Link* create_link(Link* nextval);
+int extractIntegerWords(string str);
 
 
 int main(void) {
@@ -39,15 +42,28 @@ int main(void) {
     for(int i = 0; i < c; i++) {
         cin >> caso[i].n;
         for(int j = 0; j < caso[i].n; j++) {
-            string input = NULL;
+            string input;
             getline(cin, input);
-
-            if(input.substr(0, ' ') == "insert") {
-                int it = input.substr(input.size);
-                insert(l);
+            
+            if(input.substr(0, 1) == "i") {
+                int it = extractIntegerWords(input);
+                insert(l, it);
             }
+            else if(input.substr(0, 1) == "r") {
+                remove(l);
+            }
+            else if(input.substr(0, 1) == "n") {
+                next(l);
+            }
+            else if(input.substr(0, 1) == "p") {
+                prev(l);
+            }
+            else if(input.substr(0, 1) == "c") {
+                
+            }
+
         }
-        clear();
+        //clear();
     }
 
 
@@ -76,11 +92,16 @@ List* create_list() {
 }
 
 void clear(List* l) {
-    for(int i = 0; i < l->cnt; i++) {
-        delete [] l;
-    }
-    l->cnt = 0;
-    l->curr = l->tail = l->head = create_link(NULL);    
+       Link *temp = l->head;
+       Link *next = temp->next;
+
+       while(temp->next != NULL) {
+        next = temp->next;
+        delete temp;
+        temp = next;
+       }
+       delete temp;
+       delete l;
 }
 
 void insert(List* l, int it) {
@@ -122,5 +143,35 @@ void prev(List* l) {
 void next(List* l) {
     if(l->curr != l->tail) {
         l->curr = l->curr->next;
+    }
+}
+
+int count(List* l) {
+    
+}
+
+int extractIntegerWords(string str) {
+    stringstream ss;
+ 
+    /* Storing the whole string into string stream */
+    ss << str;
+ 
+    /* Running loop till the end of the stream */
+    string temp;
+    int found;
+    while (!ss.eof()) {
+ 
+        /* extracting word by word from stream */
+        ss >> temp;
+ 
+        /* Checking the given word is integer or not */
+        if (stringstream(temp) >> found) {
+            temp = "";
+            return found;
+        }
+            //cout << found << " ";
+ 
+        /* To save from space at the end of string */
+        //temp = "";
     }
 }
