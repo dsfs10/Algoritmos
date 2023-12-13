52db408dfa682b7c2534ac4cf2964b358c868a5e
Dan01010011 <136166261+Dan01010011@users.noreply.github.com>
2023-12-13 14:37:56 -0300

    Update BST.cpp

--- BST.cpp
+++ BST.cpp
@@ -23,10 +23,16 @@ BSTNode* getmin(BSTNode* rt);
 BSTNode* deletemin(BSTNode* rt);
 int find(BST* bst, int k);
 int findhelp(BSTNode* rt, int k);
+void preorder(BSTNode* rt);
+void inorder(BSTNode* rt);
+void posorder(BSTNode* rt);
 
 
 int main(void) {
-
+    
+    
+    
+    return 0;
 }
 
 
@@ -75,21 +81,90 @@ int remove(BST* bst, int k) {
 }
 
 BSTNode* removehelp(BSTNode* rt, int k) {
+    if(rt == NULL) {
+        return NULL;
+    }
 
+    if(rt->key > k) {
+        rt->left = removehelp(rt->left, k);
+    }
+    else if(rt->key < k) {
+        rt->right = removehelp(rt->right, k);
+    }
+    else {
+        if(rt->left == NULL) {
+            return rt->right;
+        }
+        else if(rt->right == NULL) {
+            return rt->left;
+        }
+        else { // when both children are not NULL
+            BSTNode* temp = getmin(rt->right);
+            rt->element = temp->element;
+            rt->key = temp->key;
+            rt->right = deletemin(rt->right);
+        }
+    }
+
+    return rt;
 }
 
 BSTNode* getmin(BSTNode* rt) {
-
+    if(rt->left == NULL) {
+        return rt;
+    }
+    return getmin(rt->left);
 }
 
 BSTNode* deletemin(BSTNode* rt) {
+    if(rt->left == NULL) {
+        return rt->right;
+    }
+    rt->left = deletemin(rt->left);
 
+    return rt;
 }
 
 int find(BST* bst, int k) {
-
+    return findhelp(bst->root, k);
 }
 
 int findhelp(BSTNode* rt, int k) {
+    if(rt == NULL) {
+        return NULL;
+    }
 
+    if(rt->key > k) {
+        return findhelp(rt->left, k);
+    }
+    else if(rt->key == k) {
+        return rt->element;
+    }
+    else {
+        return findhelp(rt->right, k);
+    }
+}
+
+void preorder(BSTNode* rt) {
+    if(rt != NULL) {
+        // do something with rt
+        preorder(rt->left);
+        preorder(rt->right);
+    }
+}
+
+void inorder(BSTNode* rt) {
+    if(rt != NULL) {
+        inorder(rt->left);
+        // do something with rt
+        inorder(rt->right);
+    }
+}
+
+void posorder(BSTNode* rt) {
+    if(rt != NULL) {
+        posorder(rt->left);
+        posorder(rt->right);
+        // do something with rt
+    }
 }
