1e3ea986ddf76ac5abf54f9b57e231c7a729eb8f
Dan01010011 <136166261+Dan01010011@users.noreply.github.com>
2024-02-26 21:23:37 -0300

    Update Lista6A.cpp

--- Lista6A.cpp
+++ Lista6A.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <list>
+#include <vector>
 #include <iterator>
 #include <queue>
 #include <limits>
@@ -9,7 +9,7 @@
 using namespace std;
 
 typedef struct g{
-    list<pair<int, int>> *l; // adjacency list
+    vector<pair<int, int>> *l; // adjacency list
     int numEdge; // number of edges
     int n; // number of vertices
     int *Mark; // auxiliary marking array
@@ -21,13 +21,11 @@ G* create_graph(const int n);
 int n(G* g);
 int e(G* g);
 int first(G* g, int v);
-int next(G* g, int v, int w);
-int weight(G* g, int i, int j);
+int next(G* g, int v, vector<pair<int, int>>::iterator &it);
+int weight(G* g, vector<pair<int, int>>::iterator it);
 void graphTraverse(G* g, int v);
 void Dijkstra(G* g, int s);
 void setEdge(G* g, int i, int j, int w);
-void delEdge(G* g, int i, int j);
-bool isEdge(G* g, int i, int j);
 void setMark(G* g, int v, int val);
 int getMark(G* g, int v);
 
@@ -63,7 +61,7 @@ G* create_graph(int n) {
     g->n = n;
     g->Mark = new int[n];
     g->D = new int[n];
-    g->l = new list<pair<int, int>>[n];
+    g->l = new vector<pair<int, int>>[n];
     g->numEdge = 0;
     return g;
 }
@@ -84,31 +82,18 @@ int first(G* g, int v) {
     return g->l[v].front().first;
 }
 
-int next(G* g, int v, int w) {
-    list<pair<int, int>>::iterator it;
-    if(!(g->l[v].empty())) {
-        for(it = g->l[v].begin(); it != g->l[v].end(); it++) {  
-            if((*it).first == w && (*it).first != g->l[v].back().first) {
-                it++;
-                return (*it).first;
-            }
-        }
+int next(G* g, int v, vector<pair<int, int>>::iterator &it) {
+    it++;
+    if(it == g->l[v].end()) {
+        return n(g);
     }
-
-    return n(g);
+    else {
+        return (*it).first;
+    }
 }
 
-int weight(G* g, int i, int j) {
-    list<pair<int, int>>::iterator it;
-    if(!(g->l[i].empty())) {
-        for(it = g->l[i].begin(); it != g->l[i].end(); it++) {  
-            if((*it).first == j) {
-                return (*it).second;
-            }
-        }
-    }
-
-    return 0;
+int weight(G* g, vector<pair<int, int>>::iterator it) {
+    return (*it).second;
 }
 
 void graphTraverse(G* g, int v) {
@@ -124,6 +109,7 @@ void graphTraverse(G* g, int v) {
 void Dijkstra(G* g, int s) {
     int *P = new int[g->n];
     int p, v;
+    vector<pair<int, int>>::iterator it;
     priority_queue<pair<int, pair<int,int>>, vector<pair<int, pair<int,int>>>, greater<pair<int, pair<int,int>>>> H;
     for(int i = 0; i <= (n(g)-1); i++) {
         g->D[i] = numeric_limits<int>::max();
@@ -145,12 +131,13 @@ void Dijkstra(G* g, int s) {
         setMark(g, v, VISITED);
         P[v] = p;
         int w = first(g, v);
+        it = g->l[v].begin();
         while(w < n(g)) {
-            if(getMark(g, w) != VISITED && g->D[w] > g->D[v] + weight(g, v, w)) {
-                g->D[w] = g->D[v] + weight(g, v, w);
+            if(getMark(g, w) != VISITED && g->D[w] > g->D[v] + weight(g, it)) {
+                g->D[w] = g->D[v] + weight(g, it);
                 H.push(make_pair(g->D[w], make_pair(v, w)));    
             }
-            w = next(g, v, w);
+            w = next(g, v, it);
         }
     }
 }
@@ -160,16 +147,6 @@ void setEdge(G* g, int i, int j, int w) {
 
     g->l[i].push_back(make_pair(j, w));
     g->l[j].push_back(make_pair(i, w)); // only for undirected graphs
-}
-
-void delEdge(G* g, int i, int j) {
-    g->numEdge--;
-    int w = weight(g, i, j);
-    g->l[i].remove(make_pair(j, w));
-}
-
-bool isEdge(G* g, int i, int j) {
-    return true;
 }
 
 void setMark(G* g, int v, int val) {
